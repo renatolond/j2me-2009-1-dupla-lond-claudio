20f2a211df2b772d1dfad1e6d2897469cbb35b36
renato.lond <renato.lond@f39b63b8-2946-11de-906c-4b287f8c8247>
2009-05-26 03:37:44 +0000

    Ready! :)

--- trunk/ArkanoidLond/src/ArkCanvas.java
+++ trunk/ArkanoidLond/src/ArkCanvas.java
@@ -1,9 +1,10 @@
+import java.util.Random;
+
 import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Graphics;
 
-
-public class ArkCanvas extends Canvas
+public class ArkCanvas extends Canvas implements Runnable
 {
 
 	private String chave;
@@ -18,23 +19,27 @@ public class ArkCanvas extends Canvas
 	private int acceleration;
 	private int xBall;
 	private int yBall;
-	
+	private int mAtraso;
+
+	private Random rand;
+
 	private Thread bThread;
-	
+	private int yBat;
+
 	static final int BGColor = 0x0000FF;
 	static final int FontColor = 0xFFFFFF;
 	static final int BarColor = 0x00FF00;
-	static final int BallColor = 0x00FFFF;
-	
+	static final int BallColor = 0xFFFF00;
+
 	static final int hBat = 5;
 	static final int wBat = 30;
-	
-	static final int hBall = 5;
-	static final int wBall = 5;
-	
+
+	static final int hBall = 10;
+	static final int wBall = 10;
+
 	static final int leapingFactor = 8;
 	static final int yMargin = 20;
-	
+
 	public ArkCanvas()
 	{
 		acceleration = leapingFactor;
@@ -42,12 +47,18 @@ public class ArkCanvas extends Canvas
 		height = getHeight();
 		width = getWidth();
 		xbat = width / 2;
-		
-		fonte = Font.getFont(Font.FACE_PROPORTIONAL, Font.STYLE_BOLD, Font.SIZE_LARGE);
-		
-		bThread = new Thread();
+
+		fonte = Font.getFont(Font.FACE_PROPORTIONAL, Font.STYLE_BOLD,
+				Font.SIZE_LARGE);
+
+		rand = new Random();
+		rand.setSeed(10930193);
+		yBat = height - yMargin;
+
+		mAtraso = 60;
+		bThread = new Thread(this);
 		bThread.start();
-		
+
 		yBall = 0;
 		xBall = width / 2;
 	}
@@ -59,7 +70,7 @@ public class ArkCanvas extends Canvas
 		DesenhaBola(g);
 		DesenhaBastao(g);
 	}
-	
+
 	private void DesenhaFundo(Graphics g)
 	{
 		g.setColor(BGColor);
@@ -77,16 +88,16 @@ public class ArkCanvas extends Canvas
 		g.setFont(fonte);
 
 		score = String.valueOf(scored);
-		while ( score.length() < 5 )
+		while (score.length() < 5)
 			score = '0' + score;
 
 		took = String.valueOf(taken);
-		while ( took.length() < 5 )
+		while (took.length() < 5)
 			took = '0' + took;
-		
+
 		g.drawString(score, 0, 0, Graphics.TOP | Graphics.LEFT);
 		g.drawString(took, width, 0, Graphics.TOP | Graphics.RIGHT);
-		
+
 		g.setFont(tmp);
 	}
 
@@ -99,46 +110,92 @@ public class ArkCanvas extends Canvas
 	private void DesenhaBastao(Graphics g)
 	{
 		g.setColor(BarColor);
-		g.fillRect(xbat - (wBat/2), height - (yMargin + (hBat/2)), wBat, hBat);
+		g.fillRect(xbat - (wBat / 2), yBat - (hBat / 2), wBat, hBat);
 	}
 
 	public void keyPressed(int key)
 	{
 		chave = getKeyName(key);
-		if ( chave.equals("4") )
+		if (chave.equals("4")||chave.equals("LEFT"))
 		{
-			xbat -= wBat/leapingFactor;
-			if ( xbat - (wBat/2) <= 0 )
-				xbat = wBat/2;
+			xbat -= wBat / leapingFactor;
+			if (xbat - (wBat / 2) <= 0)
+				xbat = wBat / 2;
 		}
-		if ( chave.equals("6") )
+		if (chave.equals("6")||chave.equals("RIGHT"))
 		{
-			xbat += wBat/leapingFactor;
-			if ( xbat + (wBat/2) >= width )
-				xbat = width - (wBat/2);
+			xbat += wBat / leapingFactor;
+			if (xbat + (wBat / 2) >= width)
+				xbat = width - (wBat / 2);
 		}
-		if ( chave.equals("8") )
+		if (chave.equals("8"))
 			yp += 10;
-		if ( chave.equals("2") )
+		if (chave.equals("2"))
 			yp -= 10;
 		repaint();
 	}
 
-	public void ballIncrease()
+	public void run()
 	{
-		yBall += acceleration;
-		repaint();
-	}
-	
-	public class ballThread implements Runnable
-	{
-		public ArkCanvas parent;
-		
-		public void run()
+		while (true)
 		{
-			parent.ballIncrease();
+			tick();
+			yBall += acceleration;
+			repaint();
+			Collision();
+			ScoreCount();
+
+			try
+			{
+				Thread.sleep(mAtraso);
+			} catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
+	}
+
+	private void Collision()
+	{
+		int nextPlace = yBall + acceleration;
+		if (xBall >= xbat - (wBat / 2) && xBall <= xbat + (wBat / 2))
+			if (nextPlace >= yBat - (hBat / 2))
+			{
+				yBall += acceleration/2;
+				acceleration = -acceleration;
+			}
+	}
+
+	private void ScoreCount()
+	{
+		if (yBall + acceleration >= height)
+		{
+			taken++;
+			yBall = 0;
+			xBall = (rand.nextInt() % (width - wBall)) + (wBall / 2);
+			if (xBall - (wBall / 2) <= 0)
+				xBall = wBall / 2;
+			if (xBall + (wBall / 2) >= width)
+				xBall = width - (wBall / 2);
+			
+		} else if (yBall + acceleration <= 0)
+		{
+			yBall = 0;
+			xBall = (rand.nextInt() % (width - wBall)) + (wBall / 2);
+			if (xBall - (wBall / 2) <= 0)
+				xBall = wBall / 2;
+			if (xBall + (wBall / 2) >= width)
+				xBall = width - (wBall / 2);
+			acceleration = -acceleration;
+			scored++;
+		}
+	}
+
+	private void tick()
+	{
+		// TODO Auto-generated method stub
+
 	}
 
 }
