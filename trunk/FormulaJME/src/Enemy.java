2a0fa682a78b210d5d5adb5ef8f7d084151cf637
renato.lond <renato.lond@f39b63b8-2946-11de-906c-4b287f8c8247>
2009-07-07 17:27:40 +0000

    Carrinhos andando smoothly.

--- trunk/FormulaJME/src/Enemy.java
+++ trunk/FormulaJME/src/Enemy.java
@@ -52,42 +52,85 @@ public class Enemy extends Entity
 		if ( phi < 0 )
 			phi += 2*Math.PI;
 		dx = dy = 1;
-		double diff = phi - teta;
-		//if ( diff < 0.0001 ) diff += Math.PI*2;
-		double diff2 = teta - phi;
-		//if ( diff2 < 0.0001 ) diff2 += Math.PI*2;
+		double dsin = Math.sin(phi)*speed;
+		double dcos = Math.cos(phi)*speed;
+		dx = -(int)dsin;
+		dy = (int)dcos;
 		
-		if ( Math.abs(diff) > Math.abs(diff2) )
-			diff = diff2;
+		double dd = Math.sqrt(dx*dx + dy*dy);
+		double vd = Math.sqrt(vx*vx + vy*vy);
+		double dot = (vx/vd * dx/dd) + (vy/vd * dy/dd);
+		double clock = (vx*dy) - (vy*dx);
+		double gamma = mMath.acos(dot);
 		
-		if ( ++delay > 10 )
+		if ( gamma >= Math.PI/4 )
 		{
-//			System.out.println("waypoint x:" + waypoint[wayPoint].x + "waypoint y:" + waypoint[wayPoint].y);
-//			System.out.println("playerY: "+player.y+" enemyY:"+car.getY()+"playerCarY:" +player.car.getY());
-//			System.out.println("vx/vy: "+vx/vy+" vx "+vx+" vy "+vy+" teta: "+teta*180.0/Math.PI);
-			System.out.println("teta: "+teta+" carAng: "+carAng+" ang: "+ang+" phi: "+phi+"diff: " + diff*180/Math.PI);
-			delay = 0;
-			if ( diff > Math.PI/4 )
+			if ( clock < 0 )
 			{
 				car.nextFrame();
 				turnRight();
 			}
-			if ( diff < -Math.PI/4 )
+			else
 			{
 				car.prevFrame();
 				turnLeft();
 			}
 		}
 		
+		/*
+		'calculate crossproduct to determine if the target vector is clockwise or anticlockwise
+		'from the current vector: Note this is not a full crossproduct as we are only interested
+		'the information of clockwiseness, not in a vector perpendicular to both v1 and v2
+		
+		w = (v1.X * v2.Y) - (v1.Y * v2.X)
+		
+		'Add or subtract to the current angle depending on the direction
+		If w > 0 Then
+		        CurrentAngle = CurrentAngle + (ElapsedTime * RotationalVelocity)
+		Else
+		        CurrentAngle = CurrentAngle - (ElapsedTime * RotationalVelocity)
+		End If
+		
+		'Calculate dot product and thus distance (angle) between the two vectors
+		dot = (v1.X * v2.X) + (v1.Y * v2.Y)
+		theta = ArcCosine(dot)
+		
+		'If the vectors are really close togeather then just set them equal
+		If theta < (ElapsedTime * RotationalVelocity) Then
+		    CurrentAngle = TargetAngle
+		End If 
+		 */
+
+//		double diff = phi - teta;
+//		//if ( diff < 0.0001 ) diff += Math.PI*2;
+//		double diff2 = teta - phi;
+//		//if ( diff2 < 0.0001 ) diff2 += Math.PI*2;
+//		
+//		if ( Math.abs(diff) > Math.abs(diff2) )
+//			diff = diff2;
+		
+//		if ( ++delay > 10 )
+//		{
+//			System.out.println("waypoint x:" + waypoint[wayPoint].x + "waypoint y:" + waypoint[wayPoint].y);
+//			System.out.println("playerY: "+player.y+" enemyY:"+car.getY()+"playerCarY:" +player.car.getY());
+//			System.out.println("vx/vy: "+vx/vy+" vx "+vx+" vy "+vy+" teta: "+teta*180.0/Math.PI);
+//			System.out.println("teta: "+teta+" carAng: "+carAng+" ang: "+ang+" phi: "+phi+"diff: " + diff*180/Math.PI);
+//			delay = 0;
+//			if ( diff > Math.PI/4 )
+//			{
+//				car.nextFrame();
+//				turnRight();
+//			}
+//			if ( diff < -Math.PI/4 )
+//			{
+//				car.prevFrame();
+//				turnLeft();
+//			}
+//		}
+		
 		//else if ( phi - Math.PI/4 < teta )
 //			turnLeft();
 			
-
-		double dsin = Math.sin(phi)*speed;
-		double dcos = Math.cos(phi)*speed;
-		dx = -(int)dsin;
-		dy = (int)dcos;
-
 		move(dx, dy);
 	}
 	
